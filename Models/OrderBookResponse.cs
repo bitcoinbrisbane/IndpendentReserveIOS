034b15abf022beda2d61149263235612e9f42cde
Shocking <lucas@lucascullen.com>
2015-03-27 20:41:02 +1000

    Initial check-in of module iPhone

---
+++ Models/OrderBookResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace iPhone.Models
+{
+	public class OrderBookResponse
+	{
+		public String PrimaryCurrencyCode { get; set;}
+		public String SecondaryCurrencyCode { get; set;}
+
+		public OrderBookResponse ()
+		{
+		}
+	}
+}
